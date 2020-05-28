3030c43dd5d3bdbd32609c82b726aafb74d9f768
GarvinH <garvinh2001@hotmail.com>
2020-05-27 22:00:32 -0400

    Add all required links to retrieve server data from.

--- index.js
+++ index.js
@@ -21,4 +21,29 @@ app.get("/forecast/city", (req, res) => {
 
 })
 
+app.get("/forecast/coord", (req, res) => {
+    axios.get("https://api.openweathermap.org/data/2.5/forecast?lat=" + req.query.lat + "&lon="+req.query.lon+"&appid="+key).then(resp => {
+        res.send(resp.data)
+    }).catch(err => {
+        res.send(err.response.data)
+    })
+})
+
+app.get("/current/city", (req, res) => {
+    axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + req.query.city + "&appid=" + key).then(resp => {
+        res.send(resp.data)
+    }).catch(err => {
+        res.send(err.response.data)
+    })
+
+})
+
+app.get("/current/coord", (req, res) => {
+    axios.get("https://api.openweathermap.org/data/2.5/weather?lat=" + req.query.lat + "&lon="+req.query.lon+"&appid="+key).then(resp => {
+        res.send(resp.data)
+    }).catch(err => {
+        res.send(err.response.data)
+    })
+})
+
 app.listen(port, () => console.log("server running..."))
