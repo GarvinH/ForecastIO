30b2b88447542255e58e732a3c405d959477164b
GarvinH <garvinh2001@hotmail.com>
2020-06-02 15:59:30 -0400

    Updates

---
+++ precache-manifest.8f116bfea76e56edcfca81fd04a63c98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65232d0f9370c53724a159e838f4b860",
+    "url": "/ForecastIO/index.html"
+  },
+  {
+    "revision": "d5ea0555304d54fc6733",
+    "url": "/ForecastIO/static/css/main.0bd68dc6.chunk.css"
+  },
+  {
+    "revision": "4a8f416959ca3b78e8f1",
+    "url": "/ForecastIO/static/js/2.2fd84396.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ForecastIO/static/js/2.2fd84396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ea0555304d54fc6733",
+    "url": "/ForecastIO/static/js/main.44c0cd6a.chunk.js"
+  },
+  {
+    "revision": "f79e1a33d13c70ec0ad5",
+    "url": "/ForecastIO/static/js/runtime-main.987f13b7.js"
+  },
+  {
+    "revision": "6b395422fe865cd7e98403e9787e0bbe",
+    "url": "/ForecastIO/static/media/Arrow.6b395422.svg"
+  }
+]);
